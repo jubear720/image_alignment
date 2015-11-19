aea676f7442f1fab2d5330ac77e4f6ad1c206ec0
Julian Brackins <julian.brackins@mines.sdsmt.edu>
2015-11-19 02:23:17 -0700

    Good enough, I guess. Bounding box is HUGE, but it seems to work..... Not closing Issue #4 so i can fix it later, but I'm moving on to part 2

--- WarpImage.m
+++ WarpImage.m
@@ -1,22 +1,28 @@
-function image = WarpImage( I, H )
+function image = WarpImage( I, H, Padding )
 
 
     %Get dimensions of second image
     [h, w, d] = size(I);
 
+    xPad = Padding(1);
+    yPad = Padding(2);
+    h = h + yPad;
+    w = w + xPad;
+    
     %construct a meshgrid
-    [ x, y ] = meshgrid( 1:w, 1:h );
+    [ x, y ] = meshgrid( -w/2+1:w/2, -h/2+1:h/2 );
 
     %shape pixels so they're in the appropriate data type
     pixel = [ x(:)'; y(:)' ];
-    pix = [ pixel; ones(1, w*h) ];
+    pix = [ pixel; ones(1, (w)*h) ];
 
     %Apply H matrix to the all pixels in scene
     scene  = H*pix;
 
+
     % Return to original coordinate frame
-    xP=(scene(1,:)./(scene(3,:)))';
-    yP=(scene(2,:)./(scene(3,:)))';
+    xP =(scene(1,:)./(scene(3,:)))';
+    yP =(scene(2,:)./(scene(3,:)))';
     xP = reshape(xP,  w,h);
     yP = reshape(yP,  w,h);
 
--- run.m
+++ run.m
@@ -2,23 +2,32 @@ clc
 close all
 
 %[ File1, File2 ] = GetFileQuery();
-File1 = 'img\Yosemite\Yosemite1.jpg';
-File2 = 'img\Yosemite\Yosemite2.jpg';
+File1 = 'img\Yosemite\Yosemite2.jpg';
+File2 = 'img\Yosemite\Yosemite1.jpg';
 
-Points = GetPointCountQuery();
-%Points = 4;
+%Points = GetPointCountQuery();
+Points = 4
 %Getting Correspondences
 %Matlab code to manually identify corresponding points from two views.
-[p1, p2] =  GetCorrespondences( File1, File2, Points );
-%[p1, p2] = FakeCorrespondences( File1, File2, Points );
+[p1, p2] =  GetCorrespondences( File1, File2, Points )
+%[p1, p2] = FakeCorrespondences( File1, File2 );
 
 H = ComputeHomography( p1, p2 );
+fH = ComputeHomography( p1, p1 );
+
 
 I1 = im2double(imread(File1));
-I1 = rgb2gray(I1);
 I2 = im2double(imread(File2));
 H;
 
-WarpedI2 = WarpImage( I2, H );
+[ padX, padY ] = GetPadding(I1, I2, H);
+Padding = [ padX, padY ];
 
-ShowImage( WarpedI2, 'warped image');
+WarpedI1 = WarpImage( I1, fH, Padding );
+
+
+WarpedI2 = WarpImage( I2, H, Padding );
+
+%ShowImage( WarpedI2, 'warped image');
+
+CreateMosaic( WarpedI1, WarpedI2 );
